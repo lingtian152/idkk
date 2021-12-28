771cb94a14dbf5c942ba538922f2bffe1d15b3fd
凌天 <49418651+qw1324@users.noreply.github.com>
2021-12-28 15:03:30 -0500

    Create commands.lua

---
+++ commands.lua
@@ -0,0 +1,11 @@
+local commands = {
+        ["kick"] = ";kick",
+        ["ban"] = ";ban",
+        ["unban"] = ";unban",
+        ["shutdown"] = ";shutdown",
+        ["add"] = ";add",
+        ["remove"] = ";remove",
+        ["help"] = ";help"
+}
+
+return commands
